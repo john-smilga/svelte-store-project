b28738cdd30f8c4a444ee10afda405549b66644a
John <johnsmilga@gmail.com>
2019-11-26 06:56:15 -0800

    about content added

--- src/pages/About.svelte
+++ src/pages/About.svelte
@@ -1,1 +1,10 @@
-<h1>welcome to about page</h1>
+<section class="section about-section">
+  <h1 class="section-title">about us</h1>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+    repudiandae architecto qui adipisci in officiis, aperiam sequi atque
+    perferendis eos, autem maiores nisi saepe quisquam hic odio consectetur
+    nobis veritatis quasi explicabo obcaecati doloremque? Placeat ratione hic
+    aspernatur error blanditiis?
+  </p>
+</section>
